fccf09ed182fa4595121b176649d86bec1fff9e0
emanuele <emattina97@gmail.com>
2021-03-03 16:33:29 +0100

    Carico snack 6 rivisto

--- jsnack-6/assets/js/main.js
+++ jsnack-6/assets/js/main.js
@@ -1,14 +1,34 @@
-// chiedere quanti cubi calcolare
+//chiedere quanti cubi calcolare
 
 var n;
 var cubo;
 var i;
+// var arrayCubi = [];
+var x = 0;
+var y = 0;
 
 n = prompt("Quanti cubi vuoi calcolare?")
 
 for(i = 0; i < n; i++)
 {
   cubo = i * i * i;
-  document.getElementById('cubo').innerHTML = "Il cubo di " + i + " è " + cubo;
+  x = i;
+  y = cubo;
+  // arrayCubi.push(cubo)
+  document.getElementById('cubo').innerHTML += "Il cubo di " + x + " è " + y +" <br>";
   console.log(cubo);
 }
+
+// console.log(arrayCubi);
+// document.getElementById('cubo').innerHTML = arrayCubi;
+
+
+// var n;
+
+// n = prompt("Quanti cubi vuoi calcolare?");
+
+// for(var i = 0; i < n; i++)
+// {
+//   document.getElementById('cubo').innerHTML += ("Il cubo di " + i + " è " + Math.pow(i, 3) + "<br>")
+//   console.log(i, Math.pow(i, 3));
+// }
